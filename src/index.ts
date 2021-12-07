09e22426eaa30912a60f97e7ebc35cb46124f120
QuirinEcker <quirin.ecker@bajupa.com>
2021-12-07 15:22:28 +0100

    bot can now run

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-console.log("hello world")
+import {Client} from "discordx";
+import path from "path";
+import {fileURLToPath} from "url";
+import {Intents} from "discord.js";
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const srcPath = path.dirname(fileURLToPath(import.meta.url))
+
+if (process.env.GUILD_ID) {
+    const client = new Client({
+        intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MEMBERS,
+        ],
+        classes: [
+            `${srcPath}/commands/*`
+        ],
+        silent: false,
+        botGuilds: [
+
+        ]
+    })
+
+    client.once('ready', async () => {
+        await client.clearApplicationCommands()
+        await client.initApplicationCommands()
+        await client.initApplicationPermissions()
+        await client.initApplicationPermissions()
+    })
+
+    client.on('interactionCreate', async (interaction) => {
+        await client.executeInteraction(interaction)
+    })
+
+    if (process.env.DISCORD_TOKEN) {
+        client.login(process.env.DISCORD_TOKEN)
+    }
+}
+
+
