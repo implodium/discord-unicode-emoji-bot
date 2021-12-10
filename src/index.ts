a1aab8409557883d42ca5b7b98bfb30f2f8c0fca
QuirinEcker <quirin.ecker@bajupa.com>
2021-12-10 10:54:21 +0100

    restructured code and supporting now production environment

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,33 @@ import {fileURLToPath} from "url";
 import {Intents} from "discord.js";
 import * as dotenv from 'dotenv'
 
-dotenv.config()
+initEnvironment()
+const client = getClient()
+initCommands()
+await startBot()
 
-const srcPath = path.dirname(fileURLToPath(import.meta.url))
+function isProduction() {
+    return process.env.NODE_ENV && process.env.NODE_ENV === 'production'
+}
 
-if (process.env.GUILD_ID) {
-    const client = new Client({
+function initEnvironment() {
+    if (!isProduction()) {
+        dotenv.config({path: `${process.cwd()}/.env.dev`})
+    }
+}
+
+function getClient(): Client {
+    const srcPath = path.dirname(fileURLToPath(import.meta.url))
+
+    if (isProduction()) {
+        return getProductionClient(srcPath)
+    } else {
+        return getDevelopmentClient(srcPath)
+    }
+}
+
+function getProductionClient(srcPath: string): Client {
+    return new Client({
         intents: [
             Intents.FLAGS.GUILDS,
             Intents.FLAGS.GUILD_MESSAGES,
@@ -19,24 +40,46 @@ if (process.env.GUILD_ID) {
             `${srcPath}/commands/*.js`
         ],
         silent: false,
-        botGuilds: [
-            process.env.GUILD_ID
-        ]
     })
+}
 
+function getDevelopmentClient(srcPath: string): Client {
+    return new Client({
+        intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MEMBERS,
+        ],
+        classes: [
+            `${srcPath}/commands/*.js`
+        ],
+        silent: false,
+        botGuilds: process.env.BOT_GUILD ? [process.env.BOT_GUILD] : undefined
+    })
+}
+
+function initCommands() {
+    initCommandStructures()
+    initCommandInteraction()
+}
+
+function initCommandStructures() {
     client.once('ready', async () => {
         await client.clearApplicationCommands()
         await client.initApplicationCommands()
         await client.initApplicationPermissions()
     })
+}
 
+function initCommandInteraction() {
     client.on('interactionCreate', async (interaction) => {
         await client.executeInteraction(interaction)
     })
-
-    if (process.env.DISCORD_TOKEN) {
-        client.login(process.env.DISCORD_TOKEN)
-    }
 }
 
+async function startBot() {
+    if (process.env.DISCORD_TOKEN) {
+        await client.login(process.env.DISCORD_TOKEN)
+    }
+}
 
